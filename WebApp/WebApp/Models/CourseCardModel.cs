8731c8e0d57721b32b22ffd0cfa5560994d37be7
Clara <cskarler@gmail.com>
2024-05-29 23:29:52 +0200

    Courses page can get courses from CourseProvider

---
+++ WebApp/WebApp/Models/CourseCardModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Models;
+
+public class CourseCardModel
+{
+    public string Id { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string? Image { get; set; }
+    public string Author { get; set; } = null!;
+    public bool IsBestseller { get; set; }
+    public string? Hours { get; set; }
+    public string? LikesInPercent { get; set; }
+    public string? NumberOfLikes { get; set; }
+    public decimal OriginalPrice { get; set; }
+    public decimal DiscountPrice { get; set; }
+}
